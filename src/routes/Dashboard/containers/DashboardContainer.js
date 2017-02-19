e65af1d40e2f8f1ef43ac6d0bf02cf78a29df129
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-02-18 21:13:05 -0600

    Implement list functionality

--- src/routes/Dashboard/containers/DashboardContainer.js
+++ src/routes/Dashboard/containers/DashboardContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { dashboardVisitIncrement } from '../modules/dashboard'
+import { dashboardVisitIncrement, dashboardAddItem, dashboardEditItem } from '../modules/dashboard'
 
 import Dashboard from '../components/Dashboard'
 
@@ -9,7 +9,9 @@ import Dashboard from '../components/Dashboard'
 // }
 
 const mapDispatchToProps = {
-  dashboardVisitIncrement: () => dashboardVisitIncrement(1)
+  dashboardVisitIncrement: () => dashboardVisitIncrement(1),
+  dashboardAddItem: (value) => dashboardAddItem(value),
+  dashboardEditItem: (value) => dashboardEditItem(value)
 }
 
 const mapStateToProps = (state) => ({
@@ -17,16 +19,56 @@ const mapStateToProps = (state) => ({
 })
 
 class DashboardContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '',
+      editedItemIndex: null
+    }
+
+    this.inputOnChange = this.inputOnChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+    this.itemOnEdit = this.itemOnEdit.bind(this)
+  }
+
   componentDidMount() {
     this.props.dashboardVisitIncrement();
+  }
+  
+  inputOnChange(e) {
+    this.setState({ inputValue: e.target.value })
+  }
+
+  itemOnEdit(itemIndex) {
+    const editedItem = this.props.dashboard.dashboardItems[itemIndex]
+    this.setState({ inputValue: editedItem.label, editedItemIndex: itemIndex })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    const val = this.state.inputValue
+    const editedItemIndex = this.state.editedItemIndex
+    if(val && editedItemIndex !== null) {
+      this.props.dashboardEditItem({ val, editedItemIndex })
+      this.setState({ inputValue: '', editedItemIndex: null })
+    } else if(val) {
+      this.props.dashboardAddItem(val)
+      this.setState({ inputValue: '' })
+    } else {
+      alert(`Value can't be empty`)
+    }
   }
 
   render() {
     return (
-      <Dashboard {...this.props} />
+      <Dashboard {...this.props}
+        editedItemIndex={this.state.editedItemIndex}
+        itemOnEdit={this.itemOnEdit}
+        inputValue={this.state.inputValue}
+        inputOnChange={this.inputOnChange}
+        onSubmit={this.onSubmit} />
     );
   }
-
 } 
 
 
--- src/routes/Dashboard/modules/dashboard.js
+++ src/routes/Dashboard/modules/dashboard.js
@@ -2,6 +2,8 @@
 // Constants
 // ------------------------------------
 export const DASHBOARD_VISITS_COUNT = 'DASHBOARD_VISITS_COUNT'
+export const DASHBOARD_ADD_ITEM = 'DASHBOARD_ADD_ITEM'
+export const DASHBOARD_EDIT_ITEM = 'DASHBOARD_EDIT_ITEM'
 
 // ------------------------------------
 // Actions
@@ -13,21 +15,71 @@ export function dashboardVisitIncrement (value = 1) {
   }
 }
 
+export function dashboardAddItem (value) {
+  return {
+    type: DASHBOARD_ADD_ITEM,
+    payload: value
+  }
+}
+
+export function dashboardEditItem (value) {
+  return {
+    type: DASHBOARD_EDIT_ITEM,
+    payload: value
+  }
+}
+
 export const actions = {
-  dashboardVisitIncrement
+  dashboardVisitIncrement,
+  dashboardAddItem,
+  dashboardEditItem
 }
 
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
-  [DASHBOARD_VISITS_COUNT]    : (state, action) => state + action.payload,
+	[DASHBOARD_VISITS_COUNT]: (state, action) => { 
+	  return Object.assign({}, state, {
+	    visitsCount: state.visitsCount + action.payload
+	  })
+	},
+
+	[DASHBOARD_ADD_ITEM]: (state, action) => { 
+	  const mockedId = Math.floor(Date.now() / 1000)
+	  const newItem = {
+	    label: action.payload,
+	    id: mockedId
+	  }
+	  return Object.assign({}, state, {
+	    dashboardItems: [...state.dashboardItems, newItem]
+	  })
+	},
+
+	[DASHBOARD_EDIT_ITEM]: (state, action) => { 
+	  const newLabel = action.payload.val
+	  const index = action.payload.editedItemIndex
+	  let immutableDashboardItems = [...state.dashboardItems]
+	  immutableDashboardItems[index].label = newLabel
+	  return Object.assign({}, state, {
+	    dashboardItems: immutableDashboardItems
+	  })
+	}
 }
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
-const initialState = 0
+const initialState = {
+  visitsCount: 0,
+  dashboardItems: [
+    {key: 0, label: 'Angular'},
+    {key: 1, label: 'JQuery'},
+    {key: 2, label: 'Polymer'},
+    {key: 3, label: 'ReactJS'}
+  ]
+}
+
 export default function dashboardReducer (state = initialState, action) {
   const handler = ACTION_HANDLERS[action.type]
 
